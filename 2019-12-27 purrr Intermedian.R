90f91cc8f57b24ffb362aa7179c12e836380f872
Lamøy <Eirik-Andre.Lamoy@ffi.no>
2020-01-03 08:12:35 +0100

    purrr - intermedian - part 2

---
+++ 2019-12-27 purrr Intermedian.R
@@ -0,0 +1,282 @@
+
+# 1) purrr basics - a refresher
+
+library(tidyverse)
+
+# Repcap - intro  ---------------------------------------------------------
+
+# purrr: map( .x, .f, ...) / for each element of .x do .f
+
+#visit2015 <- read_csv2( "dataStMalo.csv")
+
+list <- list( a = rnorm(10,1,1), b = rnorm(100,10,2))
+
+
+map(list, sum)
+
+map( list , function(x) {sum(x)})
+
+# dbl
+map_dbl( list , sum)
+
+
+## With two arguments
+map2(list, list, function(x,y) {sum(x,y)})
+
+
+## More than two elements ->
+
+flere_lister <- list( list, list, list)
+
+
+pmap(flere_lister, function(...) {sum(...)})
+
+# A sum fra alle listene:
+pmap_dbl(flere_lister, function(...) {sum(...)})
+
+
+
+# Exercise  ---------------------------------------------------------------
+
+# Vektorer:
+visit_a <-  c(1,5,6,8,3,1,4)
+visit_b <-  c(12,51,26,48,13,21,54)
+
+to_day <- function(x) {x*24}
+
+all_visit <- list(visit_a, visit_b)
+
+# map
+all_visit_converted <- map(all_visit, to_day)
+
+map_dbl(all_visit_converted, mean)
+
+#### Three days, converted
+visit_c <- c(100:106)
+
+all_tests <- list(visit_a,visit_b,visit_c)
+#
+map(all_tests, function(x) {x*24*rnorm(length(x),1,1)}) %>% 
+  map(function(.x) {barplot(.x)})
+
+# Without print on the screen
+map(all_tests, function(x) {x*24*rnorm(length(x),1,1)}) %>% 
+  walk(function(.x) {barplot(.x)})
+
+## The sum of each element: 
+all_test_day <- map(all_tests , to_day)
+pmap( all_test_day, function(...){sum(...)})
+
+# Eqvivalent to:
+all_test_day[[1]][1] + all_test_day[[2]][1] + all_test_day[[3]][1]
+
+
+
+
+
+# Intro to mappers --------------------------------------------------------
+
+# fucus on .f: Can be 3: function, "n" or number
+
+#1) Anonymous function;
+
+map_dbl( visit_a, ~ round(mean(.x)*10))
+
+map2_dbl( visit_a, visit_b, ~ .x + .y)
+
+map2_dbl( visit_a, visit_b, ~ ..1 + ..2)
+
+
+# as_mapper
+
+round_mean <- as_mapper(~round(mean(.x)))
+
+round_mean( c(1.9,5.222, 9.9,5.9))
+
+# In map:
+map_dbl(visit_a, as_mapper(~.x*24))
+
+
+
+# Set_names to list -------------------------------------------------------
+
+mnd_visit <- function( per_mnd = 1,  min = 10, max = 40) {
+  visit_a <- list()
+  
+  for(i in 1:12) {
+    visit_a[[i]] <- runif(per_mnd, min = min, max = max);
+  }  
+  
+  return(visit_a)
+}
+
+visit_2015 <- mnd_visit(per_mnd = 10, min = 5, max = 40)
+
+visit_2016 <- mnd_visit(per_mnd = 15, min = 5, max = 40)
+
+
+visit_2015
+
+mean_mnd_2015 <- map(visit_2015, function(x) { round(mean(x), 0) })
+
+
+# Set name:
+  # Null names:
+names(mean_mnd_2015)
+
+
+length(mean_mnd_2015)
+
+# first arg. need to be the same length as the secound.
+mean_mnd_2015 <- set_names(mean_mnd_2015, month.abb)
+
+names(mean_mnd_2015)
+
+
+# Set names in a map:
+
+mean_mnd_2016 <- mnd_visit(per_mnd = 30)
+mean_mnd_2017 <- mnd_visit(per_mnd = 40)
+
+
+names(mean_mnd_2016)
+
+
+all_mean_visit <- list(mean_mnd_2015, mean_mnd_2016, mean_mnd_2017)
+
+all_mean_visit <-
+  # all year visit as list input.
+  map(all_mean_visit, function(x) {
+    # Extrapt each list from x
+    map(x, function(x) {
+      # mean all element in each list
+      mean(x)
+    })
+  })
+
+
+# Look at the new list:
+all_mean_visit[[2]]
+
+
+# give name to each mean_mnd/list
+
+
+all_mean_visit <-map(all_mean_visit, function(x) {set_names(x, month.abb)})
+
+
+for(i in 1:length(all_mean_visit) ){
+  print(str_c("Element ", i, " med navnene på elementene:"))
+  print(names(all_mean_visit[[i]]))
+}
+
+
+## When need to extract only elements that satisfy a condtion, use keep
+
+# ex. which month has over 25 visits?
+
+visit_2015 <- set_names(visit_2015, month.abb)
+
+over_22 <- keep( visit_2015, function(x) {mean(x) > 22 })
+
+names(over_22)
+
+# As mapper:
+
+limits <- as_mapper(~mean(.x) > 22)
+
+visit_2016 <- set_names(visit_2016, month.abb)
+
+over_22 <- keep(visit_2016, limits)
+
+under_22 <- discard(visit_2016, limits)
+
+names(over_22)
+names(under_22)
+
+## Keep, discard with map:
+
+head(iris);head(airquality)
+
+df_list <- list(iris, airquality)
+
+#
+map( df_list, function(x) {head(x, n = 12)} )
+
+
+map( df_list, function(x) { head(keep(x, is.factor))} )
+
+map( df_list, ~head(keep(.x, is.factor)))
+
+
+## Example three:
+all_visits <- list( c(100,22, 102, 103, 120,200, 100), 
+                    c(100,22, 20, 103, 90,240, 100),
+                    c(100,22, 2, 30, 110,101, 110))
+
+
+# Create a mapper that test if .x is more than 100 
+is_more_than_hundred <- as_mapper(~ .x > 100)
+
+
+# Use this mapper with keep() on the all_visits object 
+map(all_visits, ~ keep(.x, is_more_than_hundred))
+
+# Use the  day vector to set names to all_list
+day <- c("mon", "tue", "wed", "thu", "fri", "sat", "sun")
+full_visits_named <- map(all_visits, ~ set_names(.x, day))
+
+# Use this mapper with keep() 
+map(full_visits_named, ~ keep(.x, is_more_than_hundred))
+
+
+
+# example 4: Set_name, test if x > 100, and than filter group unde --------
+# Set the name of each subvector
+day <- c("mon", "tue", "wed", "thu", "fri", "sat", "sun")
+all_visits_named <- map(all_visits, ~ set_names(.x, day))
+
+# Create a mapper that will test if .x is over 100 
+threshold <- as_mapper( ~.x >100)
+
+# Run this mapper on the all_visits_named object: group_over
+group_over <- map(all_visits_named, ~ keep(.x, threshold))
+
+# Run this mapper on the all_visits_named object: group_under
+group_under <-  map(all_visits_named, ~ discard(.x, threshold))
+
+
+
+
+# Predicates --------------------------------------------------------------
+
+# predicate return TRUE or FALSE -> test of condiction
+
+# example: keep and discards -> return true and false
+# other:
+  # every -> test every elements, if they meet a condtion
+
+visit_a
+every( visit_a, function(x) { x > 2})
+
+every( visit_a, ~.x > 0)
+
+  # some -> test if some elements meet conditon
+some( visit_a , ~.x == 0)
+
+some( visit_a , ~.x != 0)
+
+# Create a threshold variable, set it to 160
+threshold <- 160
+
+# Create a mapper that tests if .x is over the defined threshold
+over_threshold <- as_mapper(~ .x > threshold)
+
+# Are all elements in every all_visits vectors over the defined threshold? 
+map(all_visits, ~ every(.x, over_threshold))
+
+# Are some elements in every all_visits vectors over the defined threshold? 
+map(all_visits, ~ some(.x, over_threshold))
+
+
+
